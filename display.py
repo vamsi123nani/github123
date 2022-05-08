cc0b94e95614788631a1d6b6145992b625557a6e
vamsi123nani <92877110+vamsi123nani@users.noreply.github.com>
2022-05-08 09:48:51 +0530

    Add files via upload

---
+++ display.py
@@ -0,0 +1,9 @@
+list_1 = [10, 20, 30, 40, 50]
+res_list = [x*2 for x in list_1]
+print(res_list)
+
+list_2 = [1,2,3,4,5]
+res_list1 = [x*0.1 for x in list_2]
+print(res_list1)
+
+print("The list if completed")
